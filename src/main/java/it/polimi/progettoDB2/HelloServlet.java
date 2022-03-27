a877bc255a657bdcb7ddedd89013541ac3b807ca
giovanni <giovanni.denovellis@mail.polimi.it>
2022-03-27 16:08:18 +0200

    Initial commit

---
+++ src/main/java/it/polimi/progettoDB2/HelloServlet.java
@@ -0,0 +1,35 @@
+package it.polimi.progettoDB2;
+import java.io.*;
+import java.sql.DriverManager;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+    private static final long serialVersionUID = 1L;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        final String DB_URL = "jdbc:mysql://localhost:3306/new_schema"; //Replace with your own configuration
+        final String USER = ""; //Replace with your own configuration
+        final String PASS = ""; //Replace with your own configuration
+        String result = "Connection worked";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (Exception e) {
+            result = "Connection failed"; e.printStackTrace();
+        }
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        out.println(result);
+        out.close();
+    }
+
+    public void destroy() {
+    }
+}
